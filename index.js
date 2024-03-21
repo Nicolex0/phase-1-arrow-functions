8ea4a42a8d3075c27d7d87f1c4f55976d2f917dd
Nicole Apono <napononicole@gmail.com>
2024-03-21 10:32:59 +0300

    index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function expression called divide
+const divide = function() {
+    return 2000 / 100;
+}
+
+// Arrow function called square
+const square = (num) => {
+    return num * num;
+}
+
+// Arrow function called add
+const add = (x, y) => {
+    return x + y;
+}
